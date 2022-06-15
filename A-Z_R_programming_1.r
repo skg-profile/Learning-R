e0967dc55993dea88fe4b64b5f0a991711cc3544
Shubham Kumar Gupta <107499108+skg-profile@users.noreply.github.com>
2022-06-15 18:27:44 +0530

    Add files via upload
    Types of Variables.

---
+++ A-Z_R_programming_1.r
@@ -0,0 +1,31 @@
+#integer
+
+x <- 2L
+typeof(x)
+
+
+#double
+y<- 2.5
+typeof(y)
+
+
+# by default R stores as double
+
+#complex
+
+z<-3+2i
+typeof(z)
+
+
+#character
+
+a<-"h"
+typeof(a)
+
+
+#logical
+
+q1 <- TRUE
+q2 <- F
+
+typeof(q1)
