d0d33e67dbd6c5b1912db29d689b83969f49826a
Shubham Kumar Gupta <107499108+skg-profile@users.noreply.github.com>
2022-06-15 19:26:55 +0530

    Add files via upload

---
+++ Using_Variables2.r
@@ -0,0 +1,26 @@
+A <- 10
+B <- 5
+
+C<-A+B
+
+#print 
+C
+#[1] 15
+
+#select 2 lines togather and run at once 
+var1 <-2.5
+var2<-4
+
+
+result<-var1/var2
+result
+
+answer <-sqrt(var2)
+
+
+#character operations
+
+greeting<-"Hello"
+name<-"Shubham"
+
+message<-paste(greeting,name)
