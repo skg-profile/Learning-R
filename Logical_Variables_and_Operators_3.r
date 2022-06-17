8fb50c5f9e55c2f5b00ca094d483c90e697a26a7
Shubham Kumar Gupta <107499108+skg-profile@users.noreply.github.com>
2022-06-17 23:16:42 +0530

    Add files via upload

---
+++ Logical_Variables_and_Operators_3.r
@@ -0,0 +1,31 @@
+#logical 
+#either TRUE or FALSE
+
+4<5
+#[1] TRUE
+
+
+10>100
+#[1] FALSE
+
+
+4 == 5
+[1] FALSE
+
+
+# == equal to
+# != not equal to
+# <
+4<5
+# >
+5 > 4
+# <=
+# >=
+# ! not
+result <- !(5>1)
+result
+
+# : or
+TRUE : FALSE
+# & and
+# isTRUE(x)
